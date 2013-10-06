427800e8adbf63c8522aefbbbeaba3351a9d7f55
Niklas Lochschmidt <nlochschmidt@gmail.com>
2013-10-06 14:10:29 +0200

    add wrapping function for positions

--- src/rover/Grid.java
+++ src/rover/Grid.java
@@ -6,7 +6,14 @@ public class Grid {
 	public final int height;
 
 	public Grid(int width, int height) {
+		if (width <= 0 || height <= 0) {
+			throw new IllegalArgumentException("Grid needs positive dimensions");
+		}
 		this.width = width;
 		this.height = height;
+	}
+
+	public Position wrap(Position origin) {
+		return new Position(origin.x % width, origin.y % height);
 	}
 }
--- test/rover/GridTest.java
+++ test/rover/GridTest.java
@@ -17,4 +17,49 @@ public class GridTest {
 		assertThat(grid.height, equalTo(100));
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void gridNeedsNonNegativeDimensions() {
+		try {
+			new Grid(-1, 100);
+			fail();
+		} catch (IllegalArgumentException iae) {
+		}
+		try {
+			new Grid(100, -1);
+			fail();
+		} catch (IllegalArgumentException iae) {
+		}
+		new Grid(-1, -1);
+		fail();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void gridNeedsDimensionsLargerZero() {
+		try {
+			new Grid(0, 100);
+			fail();
+		} catch (IllegalArgumentException iae) {
+		}
+		try {
+			new Grid(100, 0);
+			fail();
+		} catch (IllegalArgumentException iae) {
+		}
+		new Grid(0, 0);
+		fail();
+	}
+
+	@Test
+	public void calculateWrappedLargePosition() {
+		Grid grid = new Grid(100, 100);
+		Position[] wrapFrom = { p(0, 100), p(100, 0), p(100, 100), p(0, 0) };
+		Position expected = p(0, 0);
+		for (Position origin : wrapFrom)
+			assertThat(grid.wrap(origin), equalTo(expected));
+	}
+
+	private Position p(int x, int y) {
+		return new Position(x, y);
+	}
+
 }
