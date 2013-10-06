fe6f5de537a3b596f05eb8bcaeca9656df25dcce
Niklas Lochschmidt <nlochschmidt@gmail.com>
2013-10-06 11:11:01 +0200

    add basic mars rover with initial position

---
+++ src/rover/MarsRover.java
@@ -0,0 +1,20 @@
+package rover;
+
+public class MarsRover {
+
+	private final int x;
+	private final int y;
+
+	public MarsRover(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int x() {
+		return x;
+	}
+
+	public int y() {
+		return y;
+	}
+}
---
+++ test/rover/MarsRoverTest.java
@@ -0,0 +1,37 @@
+package rover;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/*
+ Develop an api that moves a rover around on a grid.
+ You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
+ The rover receives a character array of commands.
+ Implement commands that move the rover forward/backward (f,b).
+ Implement commands that turn the rover left/right (l,r).
+ Implement wrapping from one edge of the grid to another. (planets are spheres after all)
+ Implement obstacle detection before each move to a new square. If a given sequence of commands encounters an obstacle, the rover moves up to the last possible point and reports the obstacle.
+ Example: The rover is on a 100x100 grid at location (0, 0) and facing NORTH. The rover is given the commands "ffrff" and should end up at (2, 2)
+ Tips: use multiple classes and TDD
+ - See more at: http://craftsmanship.sv.cmu.edu/katas/mars-rover-kata#sthash.RxxKxf56.dpuf
+ */
+
+@RunWith(JUnit4.class)
+public class MarsRoverTest {
+
+	@Test
+	public void roverHasStartingPosition() {
+		MarsRover rover = new MarsRover(0, 0);
+		assertThat(rover.x(), equalTo(0));
+		assertThat(rover.y(), equalTo(0));
+
+		rover = new MarsRover(10, 15);
+		assertThat(rover.x(), equalTo(10));
+		assertThat(rover.y(), equalTo(15));
+	}
+
+}
